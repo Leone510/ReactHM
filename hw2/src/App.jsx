18998791e76e0b5bd12b6155d0709c8502cad6ec
Leone510 <meteorologsio@gmail.com>
2021-03-09 21:17:42 +0200

    HW3 without sound and ms

--- hw2/src/App.jsx
+++ hw2/src/App.jsx
@@ -99,23 +99,112 @@ import './style.css';
 const App = () => {
   const [timer, setTimer] = useState(0);
   const [timerStatus, setTimerStatus] = useState('Stop');
+  const [timeList, setTimeList] = useState([])
   const [btnsArr, setBtnsArr] = useState(
     [
       {
+        title: 'Continue',
+        color: 'btnPurple',
+        display: 'none',
+      },
+      {
         title: 'Start',
         color: 'btnGreen',
+        display: 'flex',
       },
       {
         title: 'Stop',
         color: 'btnRed',
+        display: 'flex',
       },
       {
         title: 'Reset',
         color: 'btnOrange',
+        display: 'flex',
       },
     ]
   )
+
+// ----------------- UseEffects -----------------------------
+
+  useEffect(() => {
+    const timeListFromLS = localStorage.getItem('timeList');
+    if (timeListFromLS !== null) {
+      setTimeList(JSON.parse(timeListFromLS))
+    }
+  }, [])
+
+  useEffect(() => {
+    if (timerStatus === 'Stop') {
+      timer !== 0 && (setBtnsArr(
+        [
+          {
+            title: 'Continue',
+            color: 'btnPurple',
+            display: 'flex',
+          },
+          {
+            title: 'Start',
+            color: 'btnGreen',
+            display: 'none',
+          },
+          {
+            title: 'Stop',
+            color: 'btnRed',
+            display: 'flex',
+          },
+          {
+            title: 'Reset',
+            color: 'btnOrange',
+            display: 'flex',
+          },
+        ]
+      ))
+
+      if (timer !== 0) {
+        const newTimeList = [...timeList, timer]
+        localStorage.setItem('timeList', JSON.stringify(newTimeList))
+        setTimeList(newTimeList);
+
+      }
+    }
+
+    if (timerStatus === 'Reset') {
+      setBtnsArr(
+        [
+          {
+            title: 'Continue',
+            color: 'btnPurple',
+            display: 'none',
+          },
+          {
+            title: 'Start',
+            color: 'btnGreen',
+            display: 'flex',
+          },
+          {
+            title: 'Stop',
+            color: 'btnRed',
+            display: 'flex',
+          },
+          {
+            title: 'Reset',
+            color: 'btnOrange',
+            display: 'flex',
+          },
+        ]
+      )
+
+      setTimer(0)
+      const newTimeList = [...timeList, timer]
+      localStorage.setItem('timeList', JSON.stringify(newTimeList))
+      setTimeList(newTimeList);
+    }
+
+  }, [timerStatus])
   
+// ----------------- Components -----------------------------
+
   const timerStep = () => {
     setTimer(timer + 1)
   }
@@ -165,6 +254,7 @@ const App = () => {
       <button 
         className={"button" + ` ${props.color}`}
         onClick={toDo}
+        style={{display: props.display}}
       >
         <p className="btnTitle">
           {props.children}
@@ -177,7 +267,8 @@ const App = () => {
     <div className="App">
        <main>
           {
-            timerStatus === "Stop" ?
+            (timerStatus === "Stop") ||
+            (timerStatus === "Reset") ?
             <TimerBoxStop/> :
             <TimerBoxRun/>
           }
@@ -188,13 +279,29 @@ const App = () => {
                 title={btn.title}
                 color={btn.color} 
                 key={`btn_${btn.color}`}
+                display={btn.display}
               >
                 {btn.title}
               </Button>
             )
           })}
         </div>
-        <div className="timeListBox"></div>
+        <div className="timeListBox">
+          {
+            timeList.map(item => {
+              return <p>{parserTimer(item)}</p>
+            })  
+          }
+        </div>
+        <div className="clearBox">
+          <button 
+            className="clearBtn"
+            onClick={() => {
+              localStorage.removeItem('timeList')
+              setTimeList([])
+            }}
+          >Clear timer list</button>
+        </div>
       </main>
     </div>
   );
