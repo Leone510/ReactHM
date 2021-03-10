081cd4147f4fa3d767867a1736c9d26cda491862
Leone510 <meteorologsio@gmail.com>
2021-03-10 21:18:50 +0200

    HW3 step one and two with input bug

--- hw3/src/components/step/index.js
+++ hw3/src/components/step/index.js
@@ -2,19 +2,6 @@
 import React, {useContext} from "react";
 import {StoreContext} from '../../store/context';
 
-const Input = (props) => {
-   const {storage, dispatch} = useContext(StoreContext);
-   return (
-      <label>
-         {props.children}
-         <input 
-            className={props.class}
-            placeholder={props.placeholder}
-         />
-      </label>
-   )
-}
-
 const Button = (props) => {
    return (
       <button 
@@ -31,66 +18,86 @@ const StepMid = () => {
 
    switch (storage.step) {
       case 1:
-         return (
-            <form 
+         return ([
+            <div className="step_mid">
+               <form 
                className="step_form"
-               name="userData"
+               name="userDataOne"
+               >
+                  <div className="inputWrapper">
+                     <h3>Name</h3>
+                     <input name="name" placeholder='Vasia' defaultValue={storage.userData.name}/>
+                  </div>
+                  <div className="inputWrapper">
+                     <h3>Surname</h3>
+                     <input name="surname" placeholder='Piatochkin' defaultValue={storage.userData.surname}/>
+                  </div>
+                  <div className="inputWrapper">
+                     <h3>Email</h3>
+                     <input name="email" placeholder='VasiaPiat@mail.com' defaultValue={storage.userData.email}/>
+                  </div>
+               </form>
+            </div>,
+
+            <div className="step_bot">
+            <Button 
+               toDo={() => {
+                  const form = document.forms.userDataOne;
+                  dispatch({
+                     type: 'DATA_STEP_ONE',
+                     payload: {
+                        name: form.elements.name.value,
+                        surname: form.elements.surname.value,
+                        email: form.elements.email.value,
+                     }
+                  })
+
+                  dispatch({type: 'NEXT_STEP'})
+               }}
             >
-               <label>
-                  Name:
-                  <Input
-                     placeholder='Vasia'
-                  />
-               </label>
+               NEXT
+            </Button>
+            </div>
+         ]);
+      case 2:
+         return ([
+            <div className="step_mid">
+               <form 
+               className="step_form"
+               name="userDataTwo"
+               >
+                  <div className="inputWrapper">
+                     <h3>City</h3>
+                     <input name="town" placeholder='Kharkov' defaultValue={storage.userData.town}/>
+                  </div>
+                  <div className="inputWrapper">
+                     <h3>Street</h3>
+                     <input name="street" placeholder='Rudneva str.' defaultValue={storage.userData.street}/>
+                  </div>
+                  <div className="inputWrapper">
+                     <h3>House number</h3>
+                     <input name="house" placeholder='23a' defaultValue={storage.userData.house}/>
+                  </div>
             </form>
-         );
-      case 2:
-         return (
-            <div className="step_bot">
-               <Button toDo = {() => {dispatch({type: 'PREVIOUS_STEP'})}}>PREVIOUS</Button>
-               <Button toDo={() => {dispatch({type: 'NEXT_STEP'})}}>NEXT</Button>
-            </div>
-         )
-      case 3: 
-         return (
-            <div className="step_bot">
-               <Button toDo = {() => {dispatch({type: 'PREVIOUS_STEP'})}}>PREVIOUS</Button>
-               <Button toDo={() => {dispatch({type: 'NEXT_STEP'})}}>NEXT</Button>
-            </div>
-         )
-      case 4:
-         return (
-            <div className="step_bot">
-               <Button toDo = {() => {dispatch({type: 'PREVIOUS_STEP'})}}>PREVIOUS</Button>
-               <Button toDo={() => {dispatch({type: 'NEXT_STEP'})}}>NEXT</Button>
-            </div>
-         )
-      default:
-         return (
-            <div>
-               <h1>Error!</h1>
-            </div>
-         )   
-   }
-}
+            </div>,
 
-const StepBot = () => {
-   const {storage, dispatch} = useContext(StoreContext);
-
-   switch (storage.step) {
-      case 1:
-         return (
-            <div className="step_bot">
-               <Button toDo={() => {dispatch({type: 'NEXT_STEP'})}}>NEXT</Button>
-            </div>
-         );
-      case 2:
-         return (
             <div className="step_bot">
                <Button toDo = {() => {dispatch({type: 'PREVIOUS_STEP'})}}>PREVIOUS</Button>
-               <Button toDo={() => {dispatch({type: 'NEXT_STEP'})}}>NEXT</Button>
+               <Button toDo={() => {
+                  const form = document.forms.userDataTwo;
+                  dispatch({
+                     type: 'DATA_STEP_TWO',
+                     payload: {
+                        town: form.elements.town.value,
+                        street: form.elements.street.value,
+                        house: form.elements.house.value,
+                     }
+                  })
+
+                  dispatch({type: 'NEXT_STEP'})
+               }}>NEXT</Button>
             </div>
-         )
+         ])
       case 3: 
          return (
             <div className="step_bot">
@@ -119,10 +126,9 @@ const Step = (props) => {
    return (
       <div className={`step ${props.class}`}>
          <div className="step_top">
-            <h1>Шаг: {storage.step}</h1>
+            <h1>Step: {storage.step}</h1>
          </div>
          <StepMid/>
-         <StepBot/>
       </div>
    )
 }
--- hw3/src/store/reducer.js
+++ hw3/src/store/reducer.js
@@ -19,5 +19,23 @@ export const reducer = (storage, action) => {
   if (action.type === 'PREVIOUS_STEP') {
     return {...storage, step: storage.step - 1};
   }
+  if (action.type === 'DATA_STEP_ONE') {
+    return ({...storage, userData: { 
+      ...storage.userData,
+      name: action.payload.name,
+      surname: action.payload.surname,
+      email: action.payload.email,
+    }});
+  }
+  if (action.type === 'DATA_STEP_TWO') {
+    return ({...storage, userData: { 
+      ...storage.userData,
+      town: action.payload.town,
+      street: action.payload.street,
+      house: action.payload.house,
+    }});
+  }
+
+
   return storage;
 }
